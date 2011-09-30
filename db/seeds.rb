d998b425aea8baa4a77f1c4abd9034eaaab7674c
fidel jose villero de hoyos <fidel@MacBook-de-fidel-jose-villero-de-hoyos.local>
2011-09-30 17:11:40 -0500

    arreglo de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,5 @@
 
 #user = User.create!(id: 5, email: "fideljose1708@hotmail.com", encrypted_password: "$2a$10$041FTAL3ljcv5ZNznV22h..mPwUF31H6KoqTcuykU5TE...", role: "Paciente", name: "fidel jose villero de hoyos", EPS: "saludcoop", address: "my_address", confirmation_token: nil, confirmed_at: "2011-09-30 02:35:38", confirmation_sent_at: "2011-09-30 02:35:14", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: "2011-09-30 14:15:22", sign_in_count: 9, current_sign_in_at: "2011-09-30 15:15:59", last_sign_in_at: "2011-09-30 15:13:18", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", created_at: "2011-09-30 02:35:14", updated_at: "2011-09-30 15:15:59", id_doctor: nil)
 #user.save
-user = User.create!(:email => "admin1708@hotmail.com", :password => "1234567",  :password_confirmation => "1234567", :role => 'Admin', :name => 'fidel',  :EPS => 'Saludcoop', :address => 'Kr 6 # 16 - 22', :skip_confirmation = true)
+user = User.create!(:email => "admin1708@hotmail.com", :password => "1234567",  :password_confirmation => "1234567", :role => 'Admin', :name => 'fidel',  :EPS => 'Saludcoop', :address => 'Kr 6 # 16 - 22', :skip_confirmation => true)
 #user.save
